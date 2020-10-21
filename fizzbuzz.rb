18995bab0e1a2b6319b6d9d161fff3a9ccd9b204
Tamaki Masaya <j1721mct@gmail.com>
2020-10-21 11:30:42 +0900

    make fizzbuzz.rb

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+for i int 0..99 do {
+if (i % 3 == 0 && i % 5 == 0)
+      print("FizzBuzz","\n");
+else if (i % 3 == 0) 
+      print("Fizz","\n");
+else if (i % 5 == 0) 
+      printf("Buzz","\n");
+else 
+      printf(i,"\n");
+}
